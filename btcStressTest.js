79e1252c213455027a5477843d1b06187f485e73
CalogeroMandracchia <kalos9214@gmail.com>
2017-07-19 17:08:24 +0200

    created btcStressTest.js

---
+++ btcStressTest.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { promisify } = require('util');
+const { spawn } = require('child_process');
+const spawnPromisified = promisify(spawn);
+
+const bcreg = "bitcoin-cli -conf=/home/usrBTC/regtest/bitcoin.conf";
+
+
+const out = get(bcreg + " getbalance");
+console.log(out);
+
+
+
+async function get(cmd) 
+{
+    try
+    {
+        const { stdout, stderr } = await spawnPromisified(cmd);
+        console.log('err-', cmd, stderr);
+        return stdout;
+    }
+    catch(err)
+    {
+        throw new Error('il new si può omettere ma è più bellino');
+    }
+}
