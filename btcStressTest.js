e88a12b8cc47a22a47969ffbc299f66132081657
CalogeroMandracchia <kalos9214@gmail.com>
2017-07-19 17:30:55 +0200

    fix

--- btcStressTest.js
+++ btcStressTest.js
@@ -16,7 +16,7 @@ async function get(cmd)
 {
     try
     {
-        const { stdout, stderr } = await spawnPromisified(cmd);
+        const { stdout, stderr, err } = await spawnPromisified(cmd);
         console.log('err-', cmd, stderr);
         return stdout;
     }
