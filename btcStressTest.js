ed9a48ad4393eee855f50c1c03c6400e60ef79de
CalogeroMandracchia <kalos9214@gmail.com>
2017-07-20 15:17:26 +0200

    fix

--- btcStressTest.js
+++ btcStressTest.js
@@ -9,7 +9,7 @@ let bcreg = "bitcoin-cli -conf=/home/usrBTC/regtest/bitcoin.conf";
 
 const main = async () =>
 {
-    let address = await get(bcreg +  " getnewaddress");
+    let address = await get(bcreg +  " getnewaddress | tr -d \"\012\"");
     console.log("address: " + address);
     let txidvout = JSON.parse(await get(bcreg + " listunspent | jq -r '.[0] | { txid: .txid, vout: .vout }'"));
     console.log("txidvout: " + txidvout.txid);
