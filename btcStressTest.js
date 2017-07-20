41dd2a6b7fbd1002564516110dba720c992bc0c2
CalogeroMandracchia <kalos9214@gmail.com>
2017-07-20 18:51:43 +0200

    fixing fee

--- btcStressTest.js
+++ btcStressTest.js
@@ -7,7 +7,7 @@ const execPromisified = promisify(exec);
 
 //default params
 let bcreg = "bitcoin-cli -conf=/home/usrBTC/regtest/bitcoin.conf";
-let fee = 0.00000001;
+let fee = 0.00000100;
 
 const unitTest = async () =>
 {
