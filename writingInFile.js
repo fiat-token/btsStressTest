ef5538ed565d1a6c91fe9028b2ec544ec1ade18a
CalogeroMandracchia <kalos9214@gmail.com>
2017-07-23 15:25:35 +0200

    fix

--- writingInFile.js
+++ writingInFile.js
@@ -7,7 +7,7 @@ const { log } = require('./libs');
 
 //default params
 const bcreg = "bitcoin-cli -conf=/home/usrBTC/regtest/bitcoin.conf";
-const fee = 0.01; // it's a percentage!!
+const fee = 0.05; // it's a percentage!!
 const numberOfTransactions = process.argv[2] || 1;
 const file = "listOfhashHexTransaction.txt";
 
@@ -26,7 +26,7 @@ const main = async () =>
         for(const utxo of utxos)
         {
             const hashHexTransaction = await cssTx(destionationAddress, utxo);
-            log(file, hashHexTransaction);
+            //log(file, hashHexTransaction);
         }
     }
     catch(err)
